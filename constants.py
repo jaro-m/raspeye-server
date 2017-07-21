61c19f16c9cf7d35c5a9d2c69b42412b52a8348c
Jarosław Michalski <jar0m@yandex.com>
2017-07-21 22:11:09 +0100

    updated

--- constants.py
+++ constants.py
@@ -1,14 +1,15 @@
 CAM_OPT_KEYS = {'tl_now', 'tl_delay', 'tl_nop', 'tl_starts', 'tl_ends',
-'tl_camres', 'tl_exit', 'md_exit', 'pr_exit', 'pr_camres', 'cam_res', 'cam_shtr_spd', 'cam_iso', 'cam_exp_mode',
-'cam_led', 'disk_full', 'exit', 'running'}
+                'tl_camres', 'tl_exit', 'md_exit', 'pr_exit', 'pr_camres',
+                'cam_res', 'cam_shtr_spd', 'cam_iso', 'cam_exp_mode',
+                'cam_led', 'disk_full', 'exit', 'running'}
 
 TL_NOW_VAL = {0, 1, True, False}
 CAM_RES_MAXVAL = {2592: 1944, 1920: 1080}
 CAM_SHTR_SPD_MAXVAL = 6000000
 CAM_ISO_VAL = (0, 100, 200, 320, 400, 500, 640, 800)
 CAM_EXP_MODE_VAL = ('off', 'auto', 'night', 'nightpreview',
-'backlight','spotlight', 'sports', 'snow', 'beach', 'verylong',
-'fixedfps', 'antishake', 'fireworks')
+                    'backlight','spotlight', 'sports', 'snow', 'beach', 'verylong',
+                    'fixedfps', 'antishake', 'fireworks')
 CAM_LED_VAL = {0, 1, True, False}
 EXIT_VAL = (True, False, 1, 0)
 #ACTION_VAL = ('preview', 'mo_detect', 'timelapse')
